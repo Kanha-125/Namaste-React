21af518a3b9aa18dd8e794cb4882f9f49916ada9
Kanha Patidar <kanha.patidar.125@gmail.comm>
2023-07-16 12:00:19 +0530

    "commit"

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h1",{},"hello world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
